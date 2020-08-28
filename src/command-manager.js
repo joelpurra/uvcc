a44e2697b2e01887a0347fc95f7638c4f229f0c4
Joel Purra <mig@joelpurra.se>
2020-08-28 18:07:54 +0200

    Reduce hard-coded delay before closing the camera handle
    - The initial value of 1000 was set arbitrarily.
    - The current value of 50 is also set arbitrarily, but since `uvc-control` v2 uses promises it "should be safe".
    - May need to be adjusted if users experience problems.
    - May be removed if there are no error reports.

--- src/command-manager.js
+++ src/command-manager.js
@@ -67,13 +67,12 @@ module.exports = class CommandManager {
 			.filter((commandArgument) => (commandArgument !== this._injectCameraHelperArgumentName))
 			.map((commandArgument) => runtimeConfig[commandArgument]);
 
-		// TODO: create function to get/close camera, create camera helper.
 		let camera = null;
 
 		const closeCamera = async () => {
 			if (camera) {
 				// TODO: avoid hard-coded delay.
-				await Bluebird.delay(1000);
+				await Bluebird.delay(50);
 
 				await camera.close();
 			}
@@ -99,7 +98,7 @@ module.exports = class CommandManager {
 			try {
 				await closeCamera();
 			} catch {
-				// NOTE: ignore errors when closing the camera connection.
+				// NOTE: if there were previous errors, ignore errors when closing the camera connection.
 			}
 
 			throw error;
